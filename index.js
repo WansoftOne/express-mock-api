cec88a46ec6dfdeb7afd87f7ba162ae32a515d8b
Wansoft <wansoft.developer@gmail.com>
2022-07-22 19:03:42 -0500

    Dinamic API

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
-var API_RESPONSE = 'accept';
+const TOKEN = '8a682242-3707-43c6-bc34-a784cf9af735';
+var OPTIONS = {
+    response: 'Accept',
+    withToken: true,
+};
 
 app.post('/kueskypay', (req, res) => {
     console.log(req.body)
-    res.setHeader('Authorization','Bearer 8a682242-3707-43c6-bc34-a784cf9af735')
+    if(OPTIONS.withToken) {
+        res.setHeader('Authorization',`Bearer ${TOKEN}`)
+    }
     res.send({
-        "status": API_RESPONSE
+        "status": OPTIONS.response
     })
 });
 
@@ -22,13 +28,20 @@ app.get('/kueskypay', (req, res) => {
     })
 });
 
-app.get('/api-response', (req, res) => {
-    res.send(API_RESPONSE);
+app.get('/options', (req, res) => {
+    res.send(OPTIONS);
 });
 
-app.post('/api-response', (req, res) => {
-    API_RESPONSE = req.body.value;
-    res.send("SUCCESS")
+app.post('/options', (req, res) => {
+    OPTIONS = {
+        ...OPTIONS,
+        ...req.body
+    }
+    
+    res.send({
+        status: 'SUCCESS',
+        options: OPTIONS
+    });
 });
 
 app.listen(port, () => {
