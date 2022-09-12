89279729da8f7f7c81d84071f62bec23ce552a6b
Wansoft <wansoft.developer@gmail.com>
2022-09-12 13:44:11 -0500

    Get a new JWT

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var sha256 = require('js-sha256');
 
 const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 const API_SECRET= 'ec3c1eb4-e22c-4e86-af87-feac42a78113';
+const API_KEY= 'da812b07-12bf-4bed-8a54-7ec692fe9114';
 
 var OPTIONS = {
     response: 'Accept',
@@ -46,6 +48,25 @@ app.post('/options', (req, res) => {
         status: 'SUCCESS',
         options: OPTIONS
     });
+});
+
+app.get('/jwt', (req, res) => {
+    const iat = Date.now();
+    const exp = Date.now() + 300000;
+    
+    const jti = sha256(`${API_SECRET}:${iat}`);
+    
+    
+    var jwt = require('jsonwebtoken');
+    const payload = {
+        public_key: API_KEY,
+        iat,
+        exp,
+        jti
+    } 
+    var token = jwt.sign(payload, API_SECRET, { algorithm: 'HS256' });
+
+    res.send(token)
 });
 
 app.listen(port, () => {
